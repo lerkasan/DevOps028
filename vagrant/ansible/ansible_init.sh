be56a91bda5bdf7030b4c7dad899df6e35450af5
Valeriia Lapytska <lerkasan@gmail.com>
2021-01-03 19:44:43 -0500

    updated vagrant box and packages

--- vagrant/ansible/ansible_init.sh
+++ vagrant/ansible/ansible_init.sh
@@ -3,6 +3,6 @@
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get -y dist-upgrade
-sudo apt-get -y install python python-pip
-pip install --upgrade pip
-sudo pip install ansible
+sudo apt-get -y install software-properties-common python3
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt-get -y install ansible

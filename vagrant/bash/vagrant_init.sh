cd8dd2f1bf5a84ab7fdbc979b9a4477761c590a9
Valeriia Lapytska <lerkasan@gmail.com>
2021-01-08 03:23:00 -0500

    improved bash script for deployment via vagrant

--- vagrant/bash/vagrant_init.sh
+++ vagrant/bash/vagrant_init.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 #set -e
-DB_SOURCE="aws_rds" # Options: localhost or aws_rds
+
+# DB_SOURCE options: localhost or aws_rds
+DB_SOURCE="aws_rds"
 
 export AWS_ACCESS_KEY_ID="---------------CHANGE_ME---------------"
 export AWS_SECRET_ACCESS_KEY="-----------CHANGE_ME---------------"
@@ -8,13 +10,12 @@ export AWS_DEFAULT_REGION="us-west-2"
 
 export LC_ALL="en_US.UTF-8"
 export LC_CTYPE="en_US.UTF-8"
-export JAVA_HOME="/usr/lib/jvm/java-8-oracle"
 export M2_HOME="/usr/share/maven"
 
 export DB_NAME="auradb"
 export DB_USER="aura"
 export DB_PASS="mysecretpassword"
-export DB_HOST=`ifconfig | grep "inet addr" | grep -v -e "127.0.0.1" -e "10.0.2" | awk '{print $2}' | awk -F':' '{print $2}'`
+export DB_HOST=`ifconfig | grep "inet " | grep -v -e "127.0.0.1" -e "10.0.2" | awk '{print $2}'`
 export DB_PORT="5432"
 export LOGIN_HOST="localhost"
 DB_INSTANCE_ID="vagrantdemo"
@@ -25,31 +26,91 @@ ALLOWED_LAN=`echo ${DB_HOST}/24`
 OS_USERNAME=`whoami`
 BUCKET_NAME="ansible-demo1"
 PROJECT_DIR="/home/${OS_USERNAME}/demo1"
+
+JDK_FILENAME="jdk-8u144-linux-x64.tar.gz"
+JDK_URL="s3://${BUCKET_NAME}/${JDK_FILENAME}"
+JDK_INSTALL_DIR="/usr/lib/jvm"
+
+APPLICATION_PROPERTIES="${PROJECT_DIR}/src/main/resources/application.properties"
+APPLICATION_PROPERTIES_TEMPLATE="${PROJECT_DIR}/src/main/resources/application.properties.template"
+
 LIQUIBASE_BIN_DIR="${PROJECT_DIR}/liquibase/bin"
 LIQUIBASE_PATH="/home/${OS_USERNAME}/demo1/liquibase"
 LIQUIBASE_PROPERTIES_TEMPLATE="${LIQUIBASE_PATH}/liquibase.properties.template"
 LIQUIBASE_PROPERTIES="${LIQUIBASE_PATH}/liquibase.properties"
 LIQUIBASE_FILENAME="liquibase-3.5.3-bin.tar.gz"
+LIQUIBASE_S3_URL="s3://${BUCKET_NAME}/${LIQUIBASE_FILENAME}"
 LIQUIBASE_URL="https://github.com/liquibase/liquibase/releases/download/liquibase-parent-3.5.3/liquibase-3.5.3-bin.tar.gz"
 
 POSTGRES_JDBC_DRIVER_URL="https://jdbc.postgresql.org/download/postgresql-42.1.4.jar"
 POSTGRES_JDBC_DRIVER_FILENAME="postgresql-42.1.4.jar"
+
+DOWNLOAD_DIR="/home/${OS_USERNAME}/${DEMO_DIR}/download"
 DOWNLOAD_RETRIES=5
 
-# Install Java JDK8, Maven, PostgreSQL, Python-PIP, Ansible, Boto3, AWS-cli
-sudo add-apt-repository ppa:webupd8team/java
+function download_from_s3 {
+#    let RETRIES=$3
+    (( RETRIES=$3 ))
+    if [ ! -e "$2" ]; then
+        until [ ${RETRIES} -lt 0 ] || [ -e "$2" ]; do
+            aws s3 cp "$1" "$2"
+            (( "RETRIES--" )) # let "RETRIES--"
+            sleep 5
+        done
+        if [ ! -e "$2" ]; then
+            echo "An error occurred during downloading file by URL $1"
+            exit 1
+        fi
+    else
+        echo "INFO: Skipping download. This file exists."
+    fi
+}
+
+# Install Maven, PostgreSQL, AWScli
+#sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get -y dist-upgrade
-echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
-echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
-sudo apt-get -qq -y install oracle-java8-set-default
-sudo apt-get -y install maven python python-pip mc
-sudo pip install --upgrade pip
-sudo pip install awscli
+
+# Install AWScli
+sudo apt-get -y install awscli
+
+# Download and install JDK8 - old approach
+#echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+#echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+#sudo apt-get -qq -y install oracle-java8-set-default
+#export JAVA_HOME="/usr/lib/jvm/java-8-oracle"
+
+# Download and install JDK8 - new approach
+#sudo apt-get -y install openjdk-8-jdk openjdk-8-jre
+#export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+#export JRE_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre
+
+# Download and install JDK8 from a file stored at AWS S3
+mkdir -p "${DOWNLOAD_DIR}"
+sudo mkdir -p "${JDK_INSTALL_DIR}"
+
+download_from_s3 "${JDK_URL}" "${DOWNLOAD_DIR}/${JDK_FILENAME}" ${DOWNLOAD_RETRIES}
+
+if [ -e "${DOWNLOAD_DIR}/${JDK_FILENAME}" ]; then
+    sudo tar -xzf "${DOWNLOAD_DIR}/${JDK_FILENAME}" -C "${JDK_INSTALL_DIR}"
+fi
+
+export JAVA_HOME=`find ${JDK_INSTALL_DIR} -name java | grep -v -e "openjdk" -e "jre" | head -n 1 | rev | cut -c 10- | rev`
+export PATH=${JAVA_HOME}/bin:${PATH}
+
+sudo update-alternatives --install /usr/bin/java java "${JAVA_HOME}/bin/java" 2
+sudo update-alternatives --install /usr/bin/javac javac "${JAVA_HOME}/bin/javac" 2
+sudo update-alternatives --set java "${JAVA_HOME}/bin/java"
+sudo update-alternatives --set javac "${JAVA_HOME}/bin/javac"
+
+# Install Maven, MC
+sudo apt-get -y install maven mc
 
 # If DB_SOURCE is set to localhost then install local Postgres server, else create instance at RDS
 if [[ -z `echo ${DB_SOURCE} | grep -v localhost` ]]; then
+
+    # Install Postgres
     sudo apt-get -y install postgresql
 
     # Change listen address binding to Vagrant ethernet interface to provide host machine connectivity to postgres through forwarded port
@@ -88,43 +149,39 @@ fi
 
 # Download Liquibase binaries and PostgreSQL JDBC driver
 mkdir -p ${LIQUIBASE_BIN_DIR}
-if [ ! -e "${LIQUIBASE_BIN_DIR}/${LIQUIBASE_FILENAME}" ]; then
-# -- by default wget makes 20 tries to download file if there is an error response from server (or use --tries=40 to increase retries amount).
-# -- Exceptions are server responses CONNECTIONS_REFUSED and NOT_FOUND - in these cases wget will not retry download
-#   wget "${LIQUIBASE_URL}" -O "${LIQUIBASE_BIN_DIR}/liquibase-bin.tar.gz"
-    until [  ${DOWNLOAD_RETRIES} -lt 0 ] || [ -e "${LIQUIBASE_BIN_DIR}/${LIQUIBASE_FILENAME}" ]; do
-        aws s3 cp "s3://${BUCKET_NAME}/${LIQUIBASE_FILENAME}" "${LIQUIBASE_BIN_DIR}/${LIQUIBASE_FILENAME}"
-        let "DOWNLOAD_RETRIES--"
-        sleep 5
-    done
 
-    if [ -e "${LIQUIBASE_BIN_DIR}/${LIQUIBASE_FILENAME}" ]; then
-        tar -xzf "${LIQUIBASE_BIN_DIR}/${LIQUIBASE_FILENAME}" -C ${LIQUIBASE_BIN_DIR}
-    fi
+download_from_s3 "${LIQUIBASE_S3_URL}" "${DOWNLOAD_DIR}/${LIQUIBASE_FILENAME}" ${DOWNLOAD_RETRIES}
+
+if [ -e "${DWONLOAD_DIR}/${LIQUIBASE_FILENAME}" ]; then
+    tar -xzf "${DOWNLOAD_DIR}/${LIQUIBASE_FILENAME}" -C "${LIQUIBASE_BIN_DIR}"
 fi
 
+## -- by default wget makes 20 tries to download file if there is an error response from server (or use --tries=40 to increase retries amount).
+## -- Exceptions are server responses CONNECTIONS_REFUSED and NOT_FOUND - in these cases wget will not retry download
 if [ ! -e "${LIQUIBASE_BIN_DIR}/lib/${POSTGRES_JDBC_DRIVER_FILENAME}" ]; then
-#   wget "${POSTGRES_JDBC_DRIVER_URL}" -O "${LIQUIBASE_BIN_DIR}/lib/postgresql-jdbc-driver.jar"
-    let DOWNLOAD_RETRIES=5
-    until [  ${DOWNLOAD_RETRIES} -lt 0 ] || [ -e "${LIQUIBASE_BIN_DIR}/lib/${POSTGRES_JDBC_DRIVER_FILENAME}" ]; do
-        aws s3 cp "s3://${BUCKET_NAME}/${POSTGRES_JDBC_DRIVER_FILENAME}" "${LIQUIBASE_BIN_DIR}/lib/${POSTGRES_JDBC_DRIVER_FILENAME}"
-        let "DOWNLOAD_RETRIES--"
-        sleep 5
-    done
+    wget "${POSTGRES_JDBC_DRIVER_URL}" -O "${LIQUIBASE_BIN_DIR}/lib/${POSTGRES_JDBC_DRIVER_FILENAME}"
+#    download_from_s3 "${POSTGRES_JDBC_DRIVER_URL}" "${LIQUIBASE_BIN_DIR}/lib/${POSTGRES_JDBC_DRIVER_FILENAME}" ${DOWNLOAD_RETRIES}
 fi
 
 # Update database using Liquibase
-sed "s/%LOGIN_HOST%/${LOGIN_HOST}/g" ${LIQUIBASE_PROPERTIES_TEMPLATE} |
+sed "s/%LOGIN_HOST%/${LOGIN_HOST}/g" "${LIQUIBASE_PROPERTIES_TEMPLATE}" |
     sed "s/%DB_HOST%/${DB_HOST}/g" |
     sed "s/%DB_PORT%/${DB_PORT}/g" |
     sed "s/%DB_NAME%/${DB_NAME}/g" |
     sed "s/%DB_USER%/${DB_USER}/g" |
-    sed "s/%DB_PASS%/${DB_PASS}/g" > ${LIQUIBASE_PROPERTIES}
+    sed "s/%DB_PASS%/${DB_PASS}/g" > "${LIQUIBASE_PROPERTIES}"
 
-cd ${LIQUIBASE_BIN_DIR}
+cd "${LIQUIBASE_BIN_DIR}"
 ./liquibase --changeLogFile=../changelogs/changelog-main.xml --defaultsFile=../liquibase.properties update
 
-cd ${PROJECT_DIR}
+# Update application.properties using template
+sed "s/%DB_HOST%/${DB_HOST}/g" "${APPLICATION_PROPERTIES_TEMPLATE}" |
+    sed "s/%DB_PORT%/${DB_PORT}/g" |
+    sed "s/%DB_NAME%/${DB_NAME}/g" |
+    sed "s/%DB_USER%/${DB_USER}/g" |
+    sed "s/%DB_PASS%/${DB_PASS}/g" > "${APPLICATION_PROPERTIES}"
+
+cd "${PROJECT_DIR}"
 mvn clean package
 java -jar target/*.jar
 #nohup java -jar target/*.jar &
